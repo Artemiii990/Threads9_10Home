b7629bc0823babc6021d26dec28adf06b5fa9f88
Artemi <133803304+Artemiii990@users.noreply.github.com>
2025-05-06 14:06:54 +0300

    commit by Artemka Thread9_10

--- Threads9_10Home/Program.cs
+++ Threads9_10Home/Program.cs
@@ -1,3 +1,94 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
-Console.WriteLine("Hello, World!");
+public class PrimeGenerator
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть нижню межу (або натисніть Enter для 2):");
+        string lowerBoundInput = Console.ReadLine();
+        Console.WriteLine("Введіть верхню межу (або натисніть Enter для нескінченності):");
+        string upperBoundInput = Console.ReadLine();
+
+        int lowerBound;
+        bool lowerBoundSuccess = int.TryParse(lowerBoundInput, out lowerBound);
+
+        int upperBound;
+        bool upperBoundSuccess = int.TryParse(upperBoundInput, out upperBound);
+
+
+        if (!lowerBoundSuccess && !upperBoundSuccess)
+        {
+            lowerBound = 2;
+            upperBound = int.MaxValue;
+            
+        }
+
+         // Обробка випадків, коли межі не вказані
+        if (string.IsNullOrEmpty(lowerBoundInput))
+        {
+            lowerBound = 2;
+        }
+
+        if (string.IsNullOrEmpty(upperBoundInput))
+        {
+            upperBound = int.MaxValue; 
+        }
+
+
+
+        // Важливо! Потрібно перевірити, чи нижня межа менша або дорівнює верхній.
+        if (lowerBound > upperBound && upperBound != int.MaxValue)
+        {
+            Console.WriteLine("Нижня межа повинна бути меншою або дорівнювати верхній.");
+            return;
+        }
+
+
+
+        Task.Run(() => GeneratePrimes(lowerBound, upperBound));
+
+        Console.WriteLine("Генерація завершена. Натисніть будь-яку клавішу для виходу.");
+        Console.ReadKey();
+    }
+
+    private static void GeneratePrimes(int lowerBound, int upperBound)
+    {
+        List<int> primes = new List<int>();
+        for (int num = lowerBound; num <= upperBound; num++)
+        {
+            if (IsPrime(num))
+            {
+                primes.Add(num);
+                Console.WriteLine(num);
+            }
+        }
+    }
+
+    private static bool IsPrime(int num)
+    {
+        if (num <= 1)
+        {
+            return false;
+        }
+
+        if (num <= 3)
+        {
+            return true;
+        }
+
+        if (num % 2 == 0 && num % 3 == 0)
+        {
+            return false;
+        }
+
+        for (int i = 5; i * i <= num; i = i + 6)
+        {
+            if (num % i == 0 || num % (i + 2) == 0)
+                return false;
+        }
+        return true;
+    }
+}
